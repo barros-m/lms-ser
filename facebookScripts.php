48e6aaf733b38e090cb33b919f31b0996898de74
Matheus Barros Pereira <mbarr241@fiu.edu>
2018-10-27 13:00:33 -0400

    Mailing

--- facebookScripts.php
+++ facebookScripts.php
@@ -63,69 +63,19 @@ function makeAccount() {
 }
 
 function request(){
-    //send email to Mr.Erietta?
-    // FB.login(function(response) {
-    //     if (response.authResponse) {
-    //      console.log('Welcome!  Fetching your information.... ');
-    //      FB.api('/me', function(response) {
-    //        console.log('Good to see you, ' + response.name + '.');
-    //      });
-    //     } else {
-    //      console.log('User cancelled login or did not fully authorize.');
-    //     }
-    // });
-    $.ajax({
-        type: 'POST',
-        url: 'https://mandrillapp.com/api/1.0/messages/send.json',
-        data: {
-          'key': 'fc9c2ef5f2b757e7c0510ea82560c3e6-us19',
-          'message': {
-            'from_email': email,
-            'to': [
-                {
-                  'email': 'mbarr241@fiu.edu',
-                  'name': 'Theus Barros',
-                  'type': 'to'
-                }
-              ],
-            'autotext': 'true',
-            'subject': 'It seems that it worked',
-            'html': 'hello hello, jovem'
-          }
-        }
-       }).done(function(response) {
-         console.log(response); // if you're into that sorta thing
-       });
-
-    // $.ajax({
-    //     type: “POST”,
-    //     url: “https://mandrillapp.com/api/1.0/messages/send.json”,
-    //     data: {
-    //       ‘key’: ‘YOUR API KEY HERE’,
-    //       ‘message’: {
-    //         ‘from_email’: ‘YOUR@EMAIL.HERE’,
-    //         ‘to’: [
-    //             {
-    //               ‘email’: ‘RECIPIENT_NO_1@EMAIL.HERE’,
-    //               ‘name’: ‘RECIPIENT NAME (OPTIONAL)’,
-    //               ‘type’: ‘to’
-    //             },
-    //             {
-    //               ‘email’: ‘RECIPIENT_NO_2@EMAIL.HERE’,
-    //               ‘name’: ‘ANOTHER RECIPIENT NAME (OPTIONAL)’,
-    //               ‘type’: ‘to’
-    //             }
-    //           ],
-    //         ‘autotext’: ‘true’,
-    //         ‘subject’: ‘YOUR SUBJECT HERE!’,
-    //         ‘html’: ‘YOUR EMAIL CONTENT HERE! YOU CAN USE HTML!’
-    //       }
-    //     }
-    //    }).done(function(response) {
-    //      console.log(response); // if you're into that sorta thing
-    //    });
-
-    console.log("Request sent");
+   <?php
+    $to = "matheusbarros1305@gmail.com";
+    $from = "test@gmail.com";
+    $subject = "Hello, test";
+    $message = "Hey, test. Test. Matheus. Test.";
+    
+    $body = "Jovem \n\n $message";
+    
+    $headers = "From: $from";
+    
+    mail($to,$subject,$body,$headers);
+    echo "Your Message Has Been Sent Successfully (:!!.";
+   ?>
 }
 
 </script>
