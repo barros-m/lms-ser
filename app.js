38ae4f73606a2381a3b8d6f01d0ee43bfae9fcfa
mattheusbarroos <matheusbarros1305@gmail.com>
2018-10-29 17:03:36 -0400

    add express dependency

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-const http = require('http');
+//const http = require('http');
+var express = require('express');
+//const hostname = '0.0.0.0';
+//const port = 80;
+var app = express();
+var path = require('path');
 
-const hostname = '0.0.0.0';
-const port = 80;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.
+//   res.end('Hello World\n');
+// });
 
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+app.listen(80, '0.0.0.0', 511);
 
 //loads and initialize the SDK of facebook
 (function (d, s, id) {
